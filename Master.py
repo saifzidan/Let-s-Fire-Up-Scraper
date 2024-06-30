c475bf422d1d87b62d515bdead8de0f74d9af837
saif zidan <43656447+saifzidan@users.noreply.github.com>
2024-06-30 03:59:23 +0300

    scraper

---
+++ Master.py
@@ -0,0 +1,13 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+search = input("search:")
+link = "https://www.google.com/search?client=firefox-b-d&q=" + search
+os.environ["PATH"] += r"C:\Users\saifl\Selenium drivers"
+driver = webdriver.Firefox()
+driver.get(link)
+elements = driver.find_elements(By.CSS_SELECTOR, "a")
+x = open ("Links.txt" , "w")
+for element in elements:
+    x.writelines(element.get_property("href")+'\n')
+x.close()
