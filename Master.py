0b02af097f0fa276ed54ffbd54d1c7de993eb244
saif zidan <43656447+saifzidan@users.noreply.github.com>
2024-07-01 17:49:13 +0300

    level feature

--- Master.py
+++ Master.py
@@ -1,6 +1,7 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import Worker
 search = input("search:")
 link = "https://www.google.com/search?client=firefox-b-d&q=" + search
 os.environ["PATH"] += r"C:\Users\saifl\Selenium drivers"
@@ -9,6 +10,14 @@ driver.get(link)
 elements = driver.find_elements(By.CSS_SELECTOR, "a")
 x = open ("Links.txt" , "w")
 for element in elements:
-    x.writelines(element.get_property("href")+'\n')
+    x.writelines(element.get_property("href") +'\n')
 x.close()
-driver.close()
+def grab ():
+    i = 0
+    no = input("Please enter the level")
+    while i >= no:
+        Worker.scrap()
+        link = Worker.element
+        driver = webdriver.Firefox()
+        driver.get(link)
+        elements = driver.find_elements(By.CSS_SELECTOR, "a")
